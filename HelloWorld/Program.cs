bd645f43d9cf8fbd604b3ba5bf63c1dd19faba25
Vadym <vadimart92@gmail.com>
2018-02-26 00:09:21 +0200

    Demo 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace HelloWorld
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
+		static void Main(string[] args) {
+			Console.ReadKey(true);
+			//single instance per logical app
+			ActorSystem system = ActorSystem.Create("hello-world-system");
+			//used to instantiate actor instance
+			Props props = Props.Create<HelloActor>("john");
+			//used to send messages
+			IActorRef actor = system.ActorOf(props);
+			actor.Tell("world");
+
+			Console.ReadKey(true);
+			system.Terminate();
+			system.WhenTerminated.Wait();
 		}
 	}
+
+	internal class HelloActor : UntypedActor
+	{
+		private readonly string _myName;
+		public HelloActor(string myName) {
+			_myName = myName;
+		}
+		protected override void OnReceive(object message) {
+			Console.WriteLine($"Hello {message} from {_myName}");
+		}
+	}
+
 }
