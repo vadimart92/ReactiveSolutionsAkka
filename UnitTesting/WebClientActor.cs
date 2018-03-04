7c71b275a78da06c42f64a6750cff3482265ad48
Vadym <vadimart92@gmail.com>
2018-03-04 23:12:26 +0200

    unit testing

---
+++ UnitTesting/WebClientActor.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Akka.Actor;
+using Akka.TestKit.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace UnitTesting
+{
+    public class WebClientActor : ReceiveActor
+    {
+	    public class DownloadRequest {
+		    public string Address { get; set; }
+	    }
+	    public class DownloadResult {
+		    public string Content { get; set; }
+	    }
+
+	    public WebClientActor() {
+		    IActorRef sender = ActorRefs.Nobody;
+		    Receive<DownloadRequest>(request => {
+			    sender = Sender;
+			    var client = new WebClient();
+				client.DownloadStringTaskAsync(request.Address).PipeTo(Self);
+			});
+		    Receive<string>(content => sender.Tell(new DownloadResult {
+			    Content = content
+		    }));
+	    }
+    }
+
+	[TestFixture]
+	public class WebClientActorTests : TestKit
+	{
+		[Test]
+		public void DownloadRequest() {
+			var webClient = Sys.ActorOf(Props.Create<WebClientActor>());
+			webClient.Tell(new WebClientActor.DownloadRequest { Address = "http://www.google.com" });
+			var result = ExpectMsg<WebClientActor.DownloadResult>();
+			result.Content.Should().NotBeNullOrWhiteSpace();
+		}
+	}
+}
